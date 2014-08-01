03fd175ae83b3d5929c58c8f53698ba6fdc816d2
lanhuidong <lanhuidong@gmail.com>
2014-08-02 00:28:30 +0800

    fixed bug

--- virgo-web/src/main/java/com/nexusy/virgo/web/config/WebServiceConfig.java
+++ virgo-web/src/main/java/com/nexusy/virgo/web/config/WebServiceConfig.java
@@ -1,6 +1,7 @@
 package com.nexusy.virgo.web.config;
 
 import com.nexusy.virgo.data.config.DataSourceConfig;
+import com.nexusy.virgo.data.config.MybatisConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.ImportResource;
  * @since 2013-11-16
  */
 @Configuration
-@Import(DataSourceConfig.class)
+@Import({DataSourceConfig.class, MybatisConfig.class})
 @ImportResource("/WEB-INF/applicationContext-security.xml")
 public class WebServiceConfig {
 }
