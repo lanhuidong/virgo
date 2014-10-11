fce80aabbea608abc815158ffffd4b0117781797
lanhuidong <lanhuidong@gmail.com>
2014-10-11 21:18:46 +0800

    upgrade version

--- virgo-data/src/test/java/com/nexusy/virgo/data/mapper/UserMapperTest.java
+++ virgo-data/src/test/java/com/nexusy/virgo/data/mapper/UserMapperTest.java
@@ -4,7 +4,7 @@ import com.nexusy.virgo.data.DataAppTest;
 import com.nexusy.virgo.data.model.User;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.util.Assert;
 
 import java.util.Date;
 
@@ -18,7 +18,7 @@ public class UserMapperTest extends DataAppTest {
     private UserMapper userMapper;
 
     @Test
-    public void testSave(){
+    public void testSave() {
         User user = new User();
         user.setUsername("testName");
         user.setPassword("testPassword");
@@ -27,35 +27,35 @@ public class UserMapperTest extends DataAppTest {
         user.setLastLogin(new Date());
         user.setSignTime(new Date());
         userMapper.save(user);
-        System.out.println(user.getId());
+        Assert.notNull(user.getId());
     }
 
     @Test
-    public void testGet(){
+    public void testGet() {
         User user = userMapper.get(1L);
         System.out.println(user.getUsername());
     }
 
     @Test
-    public void testFindUserByUsername(){
+    public void testFindUserByUsername() {
         User user = userMapper.findUserByUsername("lanhuidong");
         System.out.println(user.getId());
     }
 
     @Test
-    public void testUpdateLoginTime(){
+    public void testUpdateLoginTime() {
         int result = userMapper.updateLoginTime(1L, new Date());
         System.out.println(result);
     }
 
     @Test
-    public void testUpdatePassword(){
+    public void testUpdatePassword() {
         int result = userMapper.updatePassword(1L, "xxx");
         System.out.println(result);
     }
 
     @Test
-    public void testUpdateBasicInfo(){
+    public void testUpdateBasicInfo() {
         int result = userMapper.updateBasicInfo(1L, "xxx", "ooo");
         System.out.println(result);
     }
